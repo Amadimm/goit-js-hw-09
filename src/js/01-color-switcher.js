3629b9fdd47e3f8541a39434e5c20e62876cb666
Amadimm <amadeuszrolecki@gmail.com>
2023-05-07 11:24:10 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function start() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+}
+
+function stop() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+}
+
+btnStart.addEventListener('click', start);
+btnStop.addEventListener('click', stop);
