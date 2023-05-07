5063b48f9884f517c97d3136eb75b192e4d10cb0
Amadimm <amadeuszrolecki@gmail.com>
2023-05-07 15:26:56 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const firstDelay = parseInt(formData.get('delay'));
+  const nextStep = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+
+  for (let i = 0; i < amount; i++)
+    createPromise(i + 1, firstDelay + i * nextStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+});
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = Math.random() > 0.3;
+      if (result) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
